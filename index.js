d8e49fcfe453ce0d9f4c0a91ed5f14fd3fef2a42
Violet A <87049568+WordsDontMakeSense@users.noreply.github.com>
2023-08-11 09:03:55 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var head = document.getElementsByTagName('head')[0];
 
 const link = document.createElement('link');
-link.href = 'styles.css';
+link.href = 'https://raw.githubusercontent.com/WordsDontMakeSense/RainbowScrollbar/main/styles.css';
 link.rel = 'stylesheet';
 
 head.insertBefore(link, head.firstChild)
